c383ce0e3591004840099ff9533525345a5e4ade
Anton Lukin <454185+antonlukin@users.noreply.github.com>
2018-05-22 22:46:46 +0300

    update clear options function prefix

--- knife-wpmu.php
+++ knife-wpmu.php
@@ -15,7 +15,7 @@
  * @link https://core.trac.wordpress.org/ticket/31245
  * @link https://github.com/Automattic/vip-go-mu-plugins-built/blob/master/misc.php#L66-L83
  */
-function _wpcom_vip_maybe_clear_alloptions_cache( $option ) {
+function _knife_clear_alloptions_cache( $option ) {
 	if ( ! wp_installing() ) {
 		$alloptions = wp_load_alloptions(); //alloptions should be cached at this point
 		if ( isset( $alloptions[ $option ] ) ) { //only if option is among alloptions
@@ -24,6 +24,6 @@ function _wpcom_vip_maybe_clear_alloptions_cache( $option ) {
 	}
 }
 
-add_action( 'added_option',   '_wpcom_vip_maybe_clear_alloptions_cache' );
-add_action( 'updated_option', '_wpcom_vip_maybe_clear_alloptions_cache' );
-add_action( 'deleted_option', '_wpcom_vip_maybe_clear_alloptions_cache' );
+add_action( 'added_option',   '_knife_clear_alloptions_cache' );
+add_action( 'updated_option', '_knife_clear_alloptions_cache' );
+add_action( 'deleted_option', '_knife_clear_alloptions_cache' );
